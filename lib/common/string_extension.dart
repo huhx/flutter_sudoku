afd0caacbaa9930b6253a1ad5f8dc7bd9b44db04
hongxiang <gohuhx@163.com>
2022-09-14 11:44:20 +0800

    [huhx] -feat: add fromQuestion & fromAnswer & check method in SudokuResponse

--- lib/common/string_extension.dart
+++ lib/common/string_extension.dart
@@ -2,4 +2,15 @@ extension StringExtension on String {
   int toInt({int? defaultValue}) {
     return int.tryParse(this) ?? defaultValue!;
   }
+
+  List<String> chunk(int chunkSize) {
+    assert(chunkSize > 0);
+    final int chunkCount = (length / chunkSize).ceil();
+
+    return List<String>.generate(chunkCount, (index) {
+      final sliceStart = index * chunkSize;
+      final sliceEnd = sliceStart + chunkSize;
+      return substring(sliceStart, (sliceEnd < length) ? sliceEnd : length);
+    });
+  }
 }
--- lib/model/sudoku.dart
+++ lib/model/sudoku.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter_sudoku/common/string_extension.dart';
 
 class SudokuRequest extends Equatable {
   final Difficulty difficulty;
@@ -42,6 +43,23 @@ enum Difficulty {
   }
 }
 
+class SudokuCheck extends Equatable {
+  final int x;
+  final int y;
+  final int actual;
+  final int expect;
+
+  const SudokuCheck({
+    required this.x,
+    required this.y,
+    required this.actual,
+    required this.expect,
+  });
+
+  @override
+  List<Object?> get props => [x, y, actual, expect];
+}
+
 class SudokuResponse extends Equatable {
   final String question;
   final String answer;
@@ -55,4 +73,34 @@ class SudokuResponse extends Equatable {
 
   @override
   List<Object?> get props => [question, answer, difficulty];
+
+  List<List<int>> fromQuestion() {
+    final List<String> firstChunk = question.chunk(9);
+    return firstChunk.map((e) => _toInt(e)).toList();
+  }
+
+  List<List<int>> fromAnswer() {
+    final List<String> firstChunk = answer.chunk(9);
+    return firstChunk.map((e) => _toInt(e)).toList();
+  }
+
+  List<SudokuCheck> check(List<List<int>> userAnswer) {
+    final List<List<int>> answer = fromAnswer();
+    final int length = userAnswer.length;
+    List<SudokuCheck> result = [];
+
+    for (int i = 0; i < length; i++) {
+      for (int j = 0; j < userAnswer[i].length; j++) {
+        final int actual = userAnswer[i][j], expect = answer[i][j];
+        if (actual != 0 && actual != expect) {
+          result.add(SudokuCheck(x: i, y: j, actual: actual, expect: expect));
+        }
+      }
+    }
+    return result;
+  }
+
+  List<int> _toInt(String string) {
+    return string.split("").map((e) => e.toInt()).toList();
+  }
 }
