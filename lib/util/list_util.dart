d47107c40259919c795b301d893385a69fcb80b6
hongxiang <gohuhx@163.com>
2022-10-16 23:03:43 +0800

    [huhx] -feat: add highlightColor and relatedColor logic when tapped the board

--- lib/util/list_util.dart
+++ lib/util/list_util.dart
@@ -1,6 +1,18 @@
 import 'package:flutter_sudoku/model/sudoku.dart';
 
 class ListUtil {
+  static List<Point> lists = [
+    const Point(x: 0, y: 0),
+    const Point(x: 0, y: 1),
+    const Point(x: 0, y: 2),
+    const Point(x: 1, y: 0),
+    const Point(x: 1, y: 1),
+    const Point(x: 1, y: 2),
+    const Point(x: 2, y: 0),
+    const Point(x: 2, y: 1),
+    const Point(x: 2, y: 2),
+  ];
+
   static List<Point> match(List<List<int>> lists, int value) {
     List<Point> points = [];
     for (int i = 0; i < lists.length; i++) {
@@ -11,5 +23,12 @@ class ListUtil {
       }
     }
     return points;
+  }
+
+  static Set<Point> related(int x, int y) {
+    final int timeX = (x / 3).floor();
+    final int timeY = (y / 3).floor();
+
+    return lists.map((point) => Point(x: point.x + timeX * 3, y: point.y + timeY * 3)).toSet();
   }
 }
