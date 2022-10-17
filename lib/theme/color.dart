61312feaf8044e1fe1393ee93ed316f9bbeb27de
hongxiang <gohuhx@163.com>
2022-10-17 10:37:44 +0800

    [huhx] -feat: add game status logic

--- lib/theme/color.dart
+++ lib/theme/color.dart
@@ -2,5 +2,6 @@ import 'package:flutter/material.dart';
 
 const Color highlightColor = Colors.greenAccent;
 const Color relatedColor = Colors.lightGreen;
-const Color inputColor = Colors.redAccent;
+const Color inputColor = Colors.blueAccent;
+const Color errorColor = Colors.red;
 const Color selectedColor = Colors.green;
