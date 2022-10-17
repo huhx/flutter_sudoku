a577e9b136a1665a0206011f2ec8993c7e654a63
hongxiang <gohuhx@163.com>
2022-10-17 11:42:08 +0800

    [huhx] -feat: add smart tips features

--- lib/business/sudoku/sudoku_operate.dart
+++ lib/business/sudoku/sudoku_operate.dart
@@ -1,7 +1,9 @@
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sudoku/business/sudoku/sudoku_notifier.dart';
 import 'package:flutter_sudoku/component/svg_icon.dart';
 import 'package:flutter_sudoku/theme/theme.dart';
+import 'package:flutter_sudoku/util/comm_util.dart';
 
 class SudokuOperate extends StatelessWidget {
   final SudokuNotifier sudokuNotifier;
@@ -14,12 +16,41 @@ class SudokuOperate extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 20),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: const [
-          OperateItem(iconName: "operate_undo", label: "撤销"),
-          OperateItem(iconName: "operate_clear", label: "擦除"),
-          OperateItem(iconName: "operate_note", label: "笔记"),
-          OperateItem(iconName: "operate_quick_note", label: "一键笔记"),
-          OperateItem(iconName: "operate_tip", label: "提示"),
+        children: [
+          OperateItem(
+            icon: const SvgIcon(name: "operate_undo", color: themeColor),
+            label: "撤销",
+            onPressed: () {},
+          ),
+          OperateItem(
+            icon: const SvgIcon(name: "operate_clear", color: themeColor),
+            label: "擦除",
+            onPressed: () {},
+          ),
+          OperateItem(
+            icon: const SvgIcon(name: "operate_note", color: themeColor),
+            label: "笔记",
+            onPressed: () {},
+          ),
+          OperateItem(
+            icon: const SvgIcon(name: "operate_quick_note", color: themeColor),
+            label: "一键笔记",
+            onPressed: () {},
+          ),
+          OperateItem(
+            icon: Badge(
+              badgeContent: Text("${sudokuNotifier.tipCount}", style: const TextStyle(fontSize: 10)),
+              child: const SvgIcon(name: "operate_tip", color: themeColor),
+            ),
+            label: "提示",
+            onPressed: () {
+              if (sudokuNotifier.tipCount <= 0) {
+                CommUtil.toast(message: "您的提示次数已经用完");
+                return;
+              }
+              sudokuNotifier.useTip();
+            },
+          ),
         ],
       ),
     );
@@ -27,23 +58,28 @@ class SudokuOperate extends StatelessWidget {
 }
 
 class OperateItem extends StatelessWidget {
-  final String iconName;
+  final Widget icon;
   final String label;
+  final VoidCallback onPressed;
 
   const OperateItem({
     super.key,
-    required this.iconName,
+    required this.icon,
     required this.label,
+    required this.onPressed,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        SvgIcon(name: iconName, color: themeColor),
-        const SizedBox(height: 6),
-        Text(label, style: const TextStyle(fontSize: 13)),
-      ],
+    return InkWell(
+      onTap: onPressed,
+      child: Column(
+        children: <Widget>[
+          icon,
+          const SizedBox(height: 6),
+          Text(label, style: const TextStyle(fontSize: 13)),
+        ],
+      ),
     );
   }
 }
