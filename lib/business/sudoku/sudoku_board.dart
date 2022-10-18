7e26eba32a06aaf3217621e443feefa579b83b84
hongxiang <gohuhx@163.com>
2022-10-18 11:52:29 +0800

    [huhx] -feat: add sudoku note feature

--- lib/business/sudoku/sudoku_board.dart
+++ lib/business/sudoku/sudoku_board.dart
@@ -13,7 +13,7 @@ class SudokuBoard extends StatelessWidget {
         9,
         (row) => TableRow(
           children: List.generate(9, (column) {
-            return TableCell(child: SudokuCell(row, column, sudokuNotifier.content[row][column], sudokuNotifier));
+            return TableCell(child: SudokuCell(row, column, sudokuNotifier));
           }),
         ),
       ),
@@ -24,13 +24,15 @@ class SudokuBoard extends StatelessWidget {
 class SudokuCell extends StatelessWidget {
   final int row;
   final int column;
-  final int number;
   final SudokuNotifier sudokuNotifier;
 
-  const SudokuCell(this.row, this.column, this.number, this.sudokuNotifier, {super.key});
+  const SudokuCell(this.row, this.column, this.sudokuNotifier, {super.key});
 
   @override
   Widget build(BuildContext context) {
+    final int? noteValue = sudokuNotifier.noteValue(row, column);
+    final int number = sudokuNotifier.content[row][column];
+
     return InkWell(
       onTap: () => sudokuNotifier.onTapped(row, column),
       child: AspectRatio(
@@ -38,10 +40,12 @@ class SudokuCell extends StatelessWidget {
         child: Container(
           decoration: BoxDecoration(border: sudokuNotifier.getBorder(row, column), color: sudokuNotifier.getColor(row, column)),
           alignment: Alignment.center,
-          child: Text(
-            number == 0 ? "" : number.toString(),
-            style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16, color: sudokuNotifier.getTextColor(row, column)),
-          ),
+          child: sudokuNotifier.enableNotes && noteValue != null
+              ? Text("$noteValue", style: const TextStyle(fontSize: 8), textAlign: TextAlign.start)
+              : Text(
+                  number == 0 ? "" : number.toString(),
+                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16, color: sudokuNotifier.getTextColor(row, column)),
+                ),
         ),
       ),
     );
