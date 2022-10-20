6ce230416033764eb5ea19f1e3eb5999a296fab1
hongxiang <gohuhx@163.com>
2022-10-20 22:13:31 +0800

    [huhx] -feat: change method to get in some extension

--- lib/api/sudoku_api.dart
+++ lib/api/sudoku_api.dart
@@ -6,7 +6,7 @@ import 'package:flutter_sudoku/util/dio_util.dart';
 class SudokuApi {
   Future<SudokuResponse> getSudokuData(DateTime dateTime, Difficulty difficulty) async {
     const String url = "http://47.105.152.148:9095/api/sudoku";
-    final SudokuRequest request = SudokuRequest(dateTime: dateTime.toDate(), difficulty: difficulty);
+    final SudokuRequest request = SudokuRequest(dateTime: dateTime.toDate, difficulty: difficulty);
     final Response response = await RestClient.getInstance().get(url, queryParameters: request.toRequest());
 
     return SudokuResponse.fromJson(response.data);
