c49ce5f04e3de46be912a4dc1fb6b1edd0ddd12b
hongxiang <gohuhx@163.com>
2022-10-23 17:57:56 +0800

    [huhx] -feat: add test for list extension

---
+++ test/common/list_extension_test.dart
@@ -0,0 +1,46 @@
+import 'package:flutter_sudoku/common/list_extension.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group("test firstOrNull", () {
+    test("return null when list is empty", () {
+      final List<int> userList = [];
+
+      expect(userList.firstOrNull, null);
+    });
+
+    test("return first item when list is not empty", () {
+      final List<int> userList = [3, 4];
+
+      expect(userList.firstOrNull, 3);
+    });
+  });
+
+  group("test lastOrNull", () {
+    test("return null when list is empty", () {
+      final List<int> userList = [];
+
+      expect(userList.lastOrNull, null);
+    });
+
+    test("return last item when list is not empty", () {
+      final List<int> userList = [3, 4];
+
+      expect(userList.lastOrNull, 4);
+    });
+  });
+
+  group("test addOrRemove", () {
+    test("return list that remove item when contain item", () {
+      final List<String> fruitList = ["apple", "orange"];
+
+      expect(fruitList.addOrRemove("apple"), ["orange"]);
+    });
+
+    test("return list that add item when not contain item", () {
+      final List<String> fruitList = ["apple", "orange"];
+
+      expect(fruitList.addOrRemove("banana"), ["apple", "orange", "banana"]);
+    });
+  });
+}
