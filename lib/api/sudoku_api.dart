9544b453204019b5da15a143cfe469912a3e6eae
hongxiang <gohuhx@163.com>
2022-10-23 23:20:11 +0800

    [huhx] -feat: move SudokuColor and question and answer information into SudokuContent

--- lib/api/sudoku_api.dart
+++ lib/api/sudoku_api.dart
@@ -1,16 +1,15 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_sudoku/common/date_extension.dart';
 import 'package:flutter_sudoku/model/sudoku.dart';
-import 'package:flutter_sudoku/model/sudoku_info.dart';
 import 'package:flutter_sudoku/util/dio_util.dart';
 
 class SudokuApi {
-  Future<SudokuInfo> getSudokuData(DateTime dateTime, Difficulty difficulty) async {
+  Future<SudokuResponse> getSudokuData(DateTime dateTime, Difficulty difficulty) async {
     const String url = "http://47.105.152.148:9095/api/sudoku";
     final SudokuRequest request = SudokuRequest(dateTime: dateTime.toDate, difficulty: difficulty);
     final Response response = await RestClient.getInstance().get(url, queryParameters: request.toRequest());
 
-    return SudokuResponse.fromJson(response.data).toSudoku();
+    return SudokuResponse.fromJson(response.data);
   }
 }
 
