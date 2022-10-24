dafbed02429f99cbcbc18d41c39e2b7ab1633a65
hongxiang <gohuhx@163.com>
2022-10-24 15:32:41 +0800

    [huhx] -feat: customize the refresh header and footer

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'dart:io';
 
 import 'package:double_back_to_close/double_back_to_close.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 import 'business/home/home_screen.dart';
 import 'business/main/theme_provider.dart';
+import 'component/custom_load_footer.dart';
+import 'component/custom_water_drop_header.dart';
 import 'theme/theme.dart';
 import 'util/app_config.dart';
 import 'util/comm_util.dart';
@@ -43,15 +47,25 @@ class MainApp extends ConsumerStatefulWidget {
 class _MainAppState extends ConsumerState<MainApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DoubleBack(
-        onFirstBackPress: (_) => CommUtil.toast(message: "再按一次退出"),
-        child: const HomeScreen(),
+    return RefreshConfiguration(
+      headerBuilder: () => const CustomWaterDropHeader(),
+      footerBuilder: () => const CustomLoadFooter(),
+      child: MaterialApp(
+        builder: (context, child) {
+          return ScrollConfiguration(
+            behavior: const CupertinoScrollBehavior(),
+            child: child!,
+          );
+        },
+        debugShowCheckedModeBanner: false,
+        home: DoubleBack(
+          onFirstBackPress: (_) => CommUtil.toast(message: "再按一次退出"),
+          child: const HomeScreen(),
+        ),
+        theme: appThemeData[AppTheme.light],
+        darkTheme: appThemeData[AppTheme.dark],
+        themeMode: ref.watch(themeProvider).themeMode,
       ),
-      debugShowCheckedModeBanner: false,
-      theme: appThemeData[AppTheme.light],
-      darkTheme: appThemeData[AppTheme.dark],
-      themeMode: ref.watch(themeProvider).themeMode,
     );
   }
 }
