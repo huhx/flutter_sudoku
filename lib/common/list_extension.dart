6f9e995e268145f974b188bb6311edcaa1e86de8
hongxiang <gohuhx@163.com>
2022-10-24 16:53:19 +0800

    [huhx] -feat: sudoku record group by datetime

--- lib/common/list_extension.dart
+++ lib/common/list_extension.dart
@@ -15,4 +15,18 @@ extension ListExtension<T> on List<T> {
     }
     return this;
   }
+
+  Map<K, List<T>> groupBy<K>(K Function(T element) keyOf) {
+    var result = <K, List<T>>{};
+    for (var element in this) {
+      (result[keyOf(element)] ??= []).add(element);
+    }
+    return result;
+  }
+
+  Iterable<R> mapIndexed<R>(R Function(int index, T element) convert) sync* {
+    for (var index = 0; index < length; index++) {
+      yield convert(index, this[index]);
+    }
+  }
 }
