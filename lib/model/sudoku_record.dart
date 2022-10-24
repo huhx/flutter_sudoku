662614994a65457d51fa3f5453052b16ad5229b8
hongxiang <gohuhx@163.com>
2022-10-24 14:39:59 +0800

    [huhx] -fix: add some field in SudokuRecord and save sudokuRecord when user input

--- lib/model/sudoku_record.dart
+++ lib/model/sudoku_record.dart
@@ -3,26 +3,32 @@ import 'package:equatable/equatable.dart';
 import 'sudoku.dart';
 
 class SudokuRecord extends Equatable {
-  final String id;
+  final int? id;
   final int year;
   final int month;
   final int day;
   final Difficulty difficulty;
-  final SudokuStatus status;
+  final GameStatus status;
   final int duration;
+  final int tips;
+  final int startTime;
+  final int endTime;
   final int createTime;
 
   @override
   List<Object?> get props => [id, year, month, day, difficulty, status, duration, createTime];
 
   const SudokuRecord({
-    required this.id,
+    this.id,
     required this.year,
     required this.month,
     required this.day,
     required this.difficulty,
     required this.status,
     required this.duration,
+    required this.tips,
+    required this.startTime,
+    required this.endTime,
     required this.createTime,
   });
 
@@ -35,22 +41,26 @@ class SudokuRecord extends Equatable {
       'difficulty': difficulty.level,
       'status': status.name,
       'duration': duration,
+      'tips': tips,
+      'startTime': startTime,
+      'endTime': endTime,
       'createTime': createTime,
     };
   }
 
   factory SudokuRecord.fromJson(Map<String, dynamic> json) {
     return SudokuRecord(
-      id: json['id'] as String,
+      id: json['id'] as int?,
       year: json['year'] as int,
       month: json['month'] as int,
       day: json['day'] as int,
       difficulty: Difficulty.from(json['difficulty'] as int),
-      status: SudokuStatus.values.byName(json['status'] as String),
+      status: GameStatus.values.byName(json['status'] as String),
       duration: json['duration'] as int,
+      tips: json['tips'] as int,
+      startTime: json['startTime'] as int,
+      endTime: json['endTime'] as int,
       createTime: json['createTime'] as int,
     );
   }
 }
-
-enum SudokuStatus { pause, success }
