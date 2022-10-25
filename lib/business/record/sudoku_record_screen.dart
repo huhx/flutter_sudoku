85dee4510f14546202d77161771c42084018a9c2
hongxiang <gohuhx@163.com>
2022-10-25 23:00:43 +0800

    [huhx] -feat: add sudoku information in play screen

--- lib/business/record/sudoku_record_screen.dart
+++ lib/business/record/sudoku_record_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sudoku/component/appbar_back_button.dart';
+import 'package:flutter_sudoku/component/svg_action_icon.dart';
 import 'package:flutter_sudoku/model/sudoku_record.dart';
+import 'package:flutter_sudoku/util/comm_util.dart';
 
 import 'sudoku_record_board.dart';
 
@@ -15,8 +17,35 @@ class SudokuRecordScreen extends StatelessWidget {
       appBar: AppBar(
         leading: const AppbarBackButton(),
         title: const Text("数独记录详情"),
+        actions: [
+          SvgActionIcon(
+            name: "sudoku_share",
+            onTap: () => CommUtil.toBeDev(),
+          ),
+        ],
       ),
-      body: SudokuRecordBoard(sudokuRecord.sudokuInputLog),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("时间：${sudokuRecord.dateString}"),
+              Text("难度: ${sudokuRecord.difficulty.label}"),
+              Text("用时: ${sudokuRecord.secondsString}"),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("次数: ${sudokuRecord.sudokuInputLog.inputSteps}"),
+              Text("错误: ${sudokuRecord.errorCount}"),
+              Text("提示: ${sudokuRecord.tipCount}"),
+            ],
+          ),
+          SudokuRecordBoard(sudokuRecord.sudokuInputLog),
+        ],
+      ),
     );
   }
 }
