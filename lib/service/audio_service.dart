4876f108d4f7a7d857c3f9414eb2db29796b12ff
hongxiang <gohuhx@163.com>
2022-10-26 17:42:34 +0800

    [huhx] -feat: add flag to determine if play sound

--- lib/service/audio_service.dart
+++ lib/service/audio_service.dart
@@ -1,18 +1,25 @@
+import 'package:flutter_sudoku/util/prefs_util.dart';
 import 'package:just_audio/just_audio.dart';
 
 class AudioService {
   final AudioPlayer _player = AudioPlayer();
 
   Future<void> playSucess() async {
-    await playSound('success.mp3');
+    if (PrefsUtil.getIsPlaySound()) {
+      await playSound('success.mp3');
+    }
   }
 
   Future<void> playInput() async {
-    await playSound('input.mp3');
+    if (PrefsUtil.getIsPlaySound()) {
+      await playSound('input.mp3');
+    }
   }
 
   Future<void> playFail() async {
-    await playSound('fail.mp3');
+    if (PrefsUtil.getIsPlaySound()) {
+      await playSound('fail.mp3');
+    }
   }
 
   Future<void> playSound(String assetName) async {
