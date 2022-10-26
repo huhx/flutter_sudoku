65f86f9159ace70a507bdc59a699de7392de1706
hongxiang <gohuhx@163.com>
2022-10-26 09:53:43 +0800

    [huhx] -feat: create SudokuCell that used in sudoku board and record board

--- lib/business/sudoku/sudoku_board.dart
+++ lib/business/sudoku/sudoku_board.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sudoku/business/sudoku/sudoku_notifier.dart';
+import 'package:flutter_sudoku/component/sudoku_cell.dart';
 import 'package:flutter_sudoku/model/sudoku_point.dart';
 
 class SudokuBoard extends StatelessWidget {
@@ -14,67 +15,22 @@ class SudokuBoard extends StatelessWidget {
         9,
         (row) => TableRow(
           children: List.generate(9, (column) {
-            return TableCell(child: SudokuCell(Point.from(row, column), sudokuNotifier));
+            final Point point = Point.from(row, column);
+            return TableCell(
+              child: InkWell(
+                onTap: () => sudokuNotifier.onTapped(point),
+                child: SudokuCell(
+                  boxBorder: point.border,
+                  value: sudokuNotifier.getValue(point),
+                  noteValue: sudokuNotifier.getNoteValue(point),
+                  color: sudokuNotifier.getColor(point),
+                  textColor: sudokuNotifier.getTextColor(point),
+                ),
+              ),
+            );
           }),
         ),
       ),
-    );
-  }
-}
-
-class SudokuCell extends StatelessWidget {
-  final Point point;
-  final SudokuNotifier sudokuNotifier;
-
-  const SudokuCell(this.point, this.sudokuNotifier, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final List<int>? noteValue = sudokuNotifier.getNoteValue(point);
-    final int number = sudokuNotifier.getValue(point);
-
-    return InkWell(
-      onTap: () => sudokuNotifier.onTapped(point),
-      child: AspectRatio(
-        aspectRatio: 1,
-        child: Container(
-          decoration: BoxDecoration(border: point.border, color: sudokuNotifier.getColor(point)),
-          alignment: Alignment.center,
-          child: noteValue != null && noteValue.isNotEmpty
-              ? SudokuNoteCell(noteValue)
-              : SudokuNormalCell(number, sudokuNotifier.getTextColor(point)),
-        ),
-      ),
-    );
-  }
-}
-
-class SudokuNoteCell extends StatelessWidget {
-  final List<int> values;
-
-  const SudokuNoteCell(this.values, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      "$values",
-      style: const TextStyle(fontSize: 8),
-      textAlign: TextAlign.start,
-    );
-  }
-}
-
-class SudokuNormalCell extends StatelessWidget {
-  final int value;
-  final Color? color;
-
-  const SudokuNormalCell(this.value, this.color, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      value == 0 ? "" : value.toString(),
-      style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16, color: color),
     );
   }
 }
