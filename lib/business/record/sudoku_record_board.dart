fedd9c92d1f9d0390760792433bc8a8679e5b03a
hongxiang <gohuhx@163.com>
2022-10-26 14:47:39 +0800

    [huhx] -feat: move the icon widgets to SudokuRecordScreen

--- lib/business/record/sudoku_record_board.dart
+++ lib/business/record/sudoku_record_board.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sudoku/component/sudoku_cell.dart';
-import 'package:flutter_sudoku/component/svg_icon.dart';
-import 'package:flutter_sudoku/model/sudoku.dart';
 import 'package:flutter_sudoku/model/sudoku_point.dart';
-import 'package:flutter_sudoku/util/comm_util.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'data/sudoku_record_notifier.dart';
@@ -15,49 +12,24 @@ class SudokuRecordBoard extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            IconButton(
-              icon: const SvgIcon(name: 'sudoku_retry', color: Colors.green),
-              onPressed: () async {
-                final GameStatus gameStatus = await sudokuRecordModel.resetPlay();
-                if (gameStatus == GameStatus.success) {
-                  CommUtil.toast(message: "恭喜你成功过关");
-                } else if (gameStatus == GameStatus.failed) {
-                  CommUtil.toast(message: "已达到最大错误次数，游戏失败");
-                }
-              },
-            ),
-            IconButton(
-              icon: const SvgIcon(name: 'sudoku_answer', color: Colors.green),
-              onPressed: () => sudokuRecordModel.showAnswer(),
-            ),
-          ],
+    return Table(
+      children: List.generate(
+        9,
+        (row) => TableRow(
+          children: List.generate(9, (column) {
+            final Point point = Point.from(row, column);
+            return TableCell(
+              child: SudokuCell(
+                boxBorder: point.border,
+                value: sudokuRecordModel.getValue(point),
+                noteValue: sudokuRecordModel.getNoteValue(point),
+                color: sudokuRecordModel.getColor(point),
+                textColor: sudokuRecordModel.getTextColor(point),
+              ),
+            );
+          }),
         ),
-        Table(
-          children: List.generate(
-            9,
-            (row) => TableRow(
-              children: List.generate(9, (column) {
-                final Point point = Point.from(row, column);
-                return TableCell(
-                  child: SudokuCell(
-                    boxBorder: point.border,
-                    value: sudokuRecordModel.getValue(point),
-                    noteValue: sudokuRecordModel.getNoteValue(point),
-                    color: sudokuRecordModel.getColor(point),
-                    textColor: sudokuRecordModel.getTextColor(point),
-                  ),
-                );
-              }),
-            ),
-          ),
-        )
-      ],
+      ),
     );
   }
 }
