d27e19fbfcd3d252034c16ff94fee60cb29a0ed9
hongxiang <gohuhx@163.com>
2022-10-26 14:47:39 +0800

    [huhx] -feat: move the icon widgets to SudokuRecordScreen

--- lib/business/record/sudoku_record_screen.dart
+++ lib/business/record/sudoku_record_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sudoku/component/appbar_back_button.dart';
 import 'package:flutter_sudoku/component/svg_action_icon.dart';
+import 'package:flutter_sudoku/component/svg_icon.dart';
+import 'package:flutter_sudoku/model/sudoku.dart';
 import 'package:flutter_sudoku/model/sudoku_record.dart';
 import 'package:flutter_sudoku/util/comm_util.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -40,15 +42,36 @@ class SudokuRecordScreen extends HookConsumerWidget {
             ],
           ),
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              Text("输入: ${sudokuRecord.sudokuInputLog.inputSteps}"),
-              Text("笔记: ${sudokuRecord.sudokuInputLog.noteSteps}"),
-              Text("错误: ${sudokuRecord.errorCount}"),
-              Text("提示: ${sudokuRecord.tipCount}"),
+              IconButton(
+                icon: const SvgIcon(name: 'sudoku_retry', color: Colors.green),
+                onPressed: () async {
+                  final GameStatus gameStatus = await sudokuRecordModel.resetPlay();
+                  if (gameStatus == GameStatus.success) {
+                    CommUtil.toast(message: "恭喜你成功过关");
+                  } else if (gameStatus == GameStatus.failed) {
+                    CommUtil.toast(message: "已达到最大错误次数，游戏失败");
+                  }
+                },
+              ),
+              IconButton(
+                icon: const SvgIcon(name: 'sudoku_answer', color: Colors.green),
+                onPressed: () => sudokuRecordModel.showAnswer(),
+              ),
             ],
           ),
           SudokuRecordBoard(sudokuRecordModel),
+          const SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("输入: ${sudokuRecord.sudokuInputLog.inputSteps}次"),
+              Text("笔记: ${sudokuRecord.sudokuInputLog.noteSteps}次"),
+              Text("错误: ${sudokuRecord.errorCount}次"),
+              Text("提示: ${sudokuRecord.tipCount}次"),
+            ],
+          ),
         ],
       ),
     );
