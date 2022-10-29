03151c7d5eb57af57efd6d87c417cf2b56e1e44e
hongxiang <gohuhx@163.com>
2022-10-29 13:53:21 +0800

    [huhx] -feat: handle deep linking

--- lib/common/string_extension.dart
+++ lib/common/string_extension.dart
@@ -1,6 +1,12 @@
+import 'package:intl/intl.dart';
+
 extension StringExtension on String {
   int toInt({int? defaultValue}) {
     return int.tryParse(this) ?? defaultValue!;
+  }
+
+   DateTime toDate({String pattern = "yyyy-MM-dd"}) {
+    return DateFormat(pattern).parse(this);
   }
 
   List<String> chunk(int chunkSize) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:double_back_to_close/double_back_to_close.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_sudoku/business/home/sudoku_screen.dart';
+import 'package:flutter_sudoku/common/string_extension.dart';
+import 'package:flutter_sudoku/model/sudoku.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
+import 'package:uni_links/uni_links.dart';
 
-import 'business/home/home_screen.dart';
 import 'business/main/theme_provider.dart';
 import 'component/custom_load_footer.dart';
 import 'component/custom_water_drop_header.dart';
@@ -16,6 +20,8 @@ import 'theme/theme.dart';
 import 'util/app_config.dart';
 import 'util/comm_util.dart';
 import 'util/prefs_util.dart';
+
+bool _initialUriIsHandled = false;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -38,13 +44,62 @@ Future<void> _initApplicationPath() async {
 }
 
 class MainApp extends ConsumerStatefulWidget {
-  const MainApp({Key? key}) : super(key: key);
+  const MainApp({super.key});
 
   @override
   ConsumerState<ConsumerStatefulWidget> createState() => _MainAppState();
 }
 
 class _MainAppState extends ConsumerState<MainApp> {
+  Uri? _initialUri, _latestUri;
+  StreamSubscription? _sub;
+
+  @override
+  void initState() {
+    super.initState();
+    _handleIncomingLinks();
+    _handleInitialUri();
+  }
+
+  @override
+  void dispose() {
+    _sub?.cancel();
+    super.dispose();
+  }
+
+  void _handleIncomingLinks() {
+    _sub = uriLinkStream.listen((Uri? uri) {
+      if (!mounted) return;
+      print('got uri: $uri');
+      setState(() => _latestUri = uri);
+    }, onError: (Object err) {
+      if (!mounted) return;
+      print('got err: $err');
+      setState(() => _latestUri = null);
+    });
+  }
+
+  Future<void> _handleInitialUri() async {
+    if (!_initialUriIsHandled) {
+      _initialUriIsHandled = true;
+      try {
+        final uri = await getInitialUri();
+        if (uri == null) {
+          print('no initial uri');
+        } else {
+          print('got initial uri: $uri');
+        }
+        if (!mounted) return;
+        setState(() => _initialUri = uri);
+      } on PlatformException {
+        print('falied to get initial uri');
+      } on FormatException catch (_) {
+        if (!mounted) return;
+        print('malformed initial uri');
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return RefreshConfiguration(
@@ -60,12 +115,31 @@ class _MainAppState extends ConsumerState<MainApp> {
         debugShowCheckedModeBanner: false,
         home: DoubleBack(
           onFirstBackPress: (_) => CommUtil.toast(message: "再按一次退出"),
-          child: const HomeScreen(),
+          child: _buildWidget(),
         ),
         theme: appThemeData[AppTheme.light],
         darkTheme: appThemeData[AppTheme.dark],
         themeMode: ref.watch(themeProvider).themeMode,
       ),
     );
+  }
+
+  Widget _buildWidget() {
+    if (_initialUri != null) {
+      final queryParams = _initialUri?.queryParametersAll.entries.toList();
+      final DateTime dateTime = queryParams![0].value[0].toDate();
+      final Difficulty difficulty = Difficulty.from(queryParams[1].value[0].toInt());
+
+      return SudokuScreen(dateTime, difficulty);
+    }
+
+    if (_latestUri != null) {
+      final queryParams = _latestUri?.queryParametersAll.entries.toList();
+      final DateTime dateTime = queryParams![0].value[0].toDate();
+      final Difficulty difficulty = Difficulty.from(queryParams[1].value[0].toInt());
+
+      return SudokuScreen(dateTime, difficulty);
+    }
+    return SudokuScreen(DateTime.now(), Difficulty.a);
   }
 }
