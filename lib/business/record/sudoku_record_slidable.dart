21e9ce38be9d79ecb5020f4d39434743c95f12d6
hongxiang <gohuhx@163.com>
2022-11-01 14:20:28 +0800

    [huhx] -feat: add flutter_slidable and implement the ui for sudoku record

---
+++ lib/business/record/sudoku_record_slidable.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:flutter_sudoku/model/sudoku_record.dart';
+import 'package:flutter_sudoku/util/comm_util.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'sudoku_record_list_screen.dart';
+
+class SudokuRecordSlidable extends ConsumerWidget {
+  final SudokuRecord sudokuRecord;
+
+  const SudokuRecordSlidable(this.sudokuRecord, {super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Slidable(
+      key: ValueKey(sudokuRecord.id),
+      endActionPane: ActionPane(
+        motion: const ScrollMotion(),
+        children: [
+          SlidableAction(
+            onPressed: (_) => CommUtil.toBeDev(),
+            backgroundColor: const Color(0xFFFE4A49),
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            label: 'Delete',
+          ),
+          SlidableAction(
+            onPressed: (_) => CommUtil.toBeDev(),
+            backgroundColor: const Color(0xFF21B7CA),
+            foregroundColor: Colors.white,
+            icon: Icons.share,
+            label: 'Share',
+          ),
+        ],
+      ),
+      child: SudokuRecordItem(sudokuRecord),
+    );
+  }
+}
