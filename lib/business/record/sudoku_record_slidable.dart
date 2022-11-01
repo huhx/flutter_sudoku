f7355cccba373967c6a20bce05b1e5055056baa0
hongxiang <gohuhx@163.com>
2022-11-01 16:08:11 +0800

    [huhx] -feat: move SudokuRecordItem into SudokuRecordSlidable

--- lib/business/record/sudoku_record_slidable.dart
+++ lib/business/record/sudoku_record_slidable.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:flutter_sudoku/api/sudoku_record_api.dart';
 import 'package:flutter_sudoku/common/context_extension.dart';
+import 'package:flutter_sudoku/component/list_tile_trailing.dart';
+import 'package:flutter_sudoku/component/text_icon.dart';
 import 'package:flutter_sudoku/model/sudoku_record.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import 'sudoku_record_list_screen.dart';
+import 'sudoku_record_screen.dart';
 
 class SudokuRecordSlidable extends ConsumerWidget {
   final SudokuRecord sudokuRecord;
@@ -41,5 +43,51 @@ class SudokuRecordSlidable extends ConsumerWidget {
       ),
       child: SudokuRecordItem(sudokuRecord),
     );
+  }
+}
+
+class SudokuRecordItem extends StatelessWidget {
+  final SudokuRecord sudokuRecord;
+
+  const SudokuRecordItem(this.sudokuRecord, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        onTap: () => context.goto(SudokuRecordScreen(sudokuRecord)),
+        leading: CircleAvatar(
+          backgroundColor: _buildColor(sudokuRecord),
+          child: Text("${sudokuRecord.duration}"),
+        ),
+        title: Text(sudokuRecord.startString),
+        subtitle: Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Row(
+            children: [
+              TextIcon(icon: "game_status", text: sudokuRecord.gameStatus.label),
+              const SizedBox(width: 16),
+              TextIcon(icon: "user_tips", text: "${sudokuRecord.tipCount}次"),
+              const SizedBox(width: 16),
+              TextIcon(icon: "error", text: "${sudokuRecord.errorCount}次"),
+              const SizedBox(width: 16),
+              TextIcon(icon: "difficulty", text: sudokuRecord.difficulty.label),
+            ],
+          ),
+        ),
+        trailing: const ListTileTrailing(),
+      ),
+    );
+  }
+
+  Color _buildColor(SudokuRecord sudokuRecord) {
+    final int duration = sudokuRecord.duration;
+    if (duration > 15 * 60) {
+      return Colors.red;
+    } else if (duration > 5 * 60) {
+      return Colors.blue;
+    } else {
+      return Colors.green;
+    }
   }
 }
