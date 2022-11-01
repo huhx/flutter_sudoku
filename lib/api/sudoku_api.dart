3540cf30174e5e0cfcf87a88f8967c537412ed89
hongxiang <gohuhx@163.com>
2022-11-01 11:49:13 +0800

    [huhx] -feat: add env file to store the sudoku server url

--- lib/api/sudoku_api.dart
+++ lib/api/sudoku_api.dart
@@ -1,13 +1,13 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_sudoku/common/date_extension.dart';
+import 'package:flutter_sudoku/common/env.dart';
 import 'package:flutter_sudoku/model/sudoku.dart';
 import 'package:flutter_sudoku/util/dio_util.dart';
 
 class SudokuApi {
   Future<SudokuResponse> getSudokuData(DateTime dateTime, Difficulty difficulty) async {
-    const String url = "sudokuUrl";
     final SudokuRequest request = SudokuRequest(dateTime: dateTime.toDate, difficulty: difficulty);
-    final Response response = await RestClient.getInstance().get(url, queryParameters: request.toRequest());
+    final Response response = await RestClient.getInstance().get(Env.sudokuUrl, queryParameters: request.toRequest());
 
     return SudokuResponse.fromJson(response.data);
   }
