a55fc69f47d5b532d0fb825421b08918e72a2590
hongxiang <gohuhx@163.com>
2023-02-11 18:12:48 +0800

    [huhx] -feat: disable scroll for the DrawerHeader

--- lib/business/home/sudoku_drawer.dart
+++ lib/business/home/sudoku_drawer.dart
@@ -16,8 +16,7 @@ class SudokuDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
+      child: Column(
         children: [
           DrawerHeader(
             decoration: BoxDecoration(color: Theme.of(context).appBarTheme.backgroundColor),
@@ -25,74 +24,81 @@ class SudokuDrawer extends StatelessWidget {
               alignment: Alignment.centerLeft,
               child: Text(
                 "每日数独",
-                style: Theme.of(context).textTheme.bodyMedium?.copyWith(fontSize: 24),
+                style: Theme.of(context).textTheme.bodyMedium?.copyWith(fontSize: 24, color: Colors.white),
               ),
             ),
           ),
-          ListTile(
-            leading: const SvgIcon(name: "item_onboarding"),
-            title: Text("玩法介绍", style: Theme.of(context).textTheme.bodyMedium),
-            onTap: () {
-              context.pop();
-              context.goto(const OnboardScreen());
-            },
-          ),
-          ListTile(
-            leading: const SvgIcon(name: "item_ analyze"),
-            title: Text("统计", style: Theme.of(context).textTheme.bodyMedium),
-            onTap: () {
-              context.pop();
-              context.goto(const SudokuStatisticsScreen());
-            },
-          ),
-          ListTile(
-            leading: const SvgIcon(name: "item_record"),
-            title: Text("记录", style: Theme.of(context).textTheme.bodyMedium),
-            onTap: () {
-              context.pop();
-              context.goto(const SudokuRecordListScreen());
-            },
-          ),
-          ListTile(
-            leading: const SvgIcon(name: "sudoku_share"),
-            title: Text("分享", style: Theme.of(context).textTheme.bodyMedium),
-            onTap: () {
-              context.pop();
-              context.share(title: '分享一款非常棒的数独游戏, 休闲益智.', subject: "sudoku");
-            },
-          ),
-          ListTile(
-            leading: const SvgIcon(name: "item_about"),
-            title: Text("关于", style: Theme.of(context).textTheme.bodyMedium),
-            onTap: () {
-              context.pop();
-              context.goto(const SudokuAboutScreen());
-            },
-          ),
-          ListTile(
-            leading: const SvgIcon(name: "item_feedback"),
-            title: Text("意见反馈", style: Theme.of(context).textTheme.bodyMedium),
-            onTap: () async {
-              context.pop();
-              final Uri mail = Uri(
-                scheme: 'mailto',
-                path: 'gohuhx@gmail.com',
-                query: 'subject=[数独]意见反馈&body=意见反馈:',
-              );
-              if (await canLaunchUrl(mail)) {
-                await launchUrl(mail);
-              } else {
-                Fluttertoast.showToast(msg: 'Can not open email app.');
-              }
-            },
-          ),
-          ListTile(
-            leading: const SvgIcon(name: "sudoku_setting"),
-            title: Text("设置", style: Theme.of(context).textTheme.bodyMedium),
-            onTap: () {
-              context.pop();
-              context.goto(const SudokuSettingScreen());
-            },
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.symmetric(vertical: 0),
+              children: [
+                ListTile(
+                  leading: const SvgIcon(name: "item_onboarding"),
+                  title: Text("玩法介绍", style: Theme.of(context).textTheme.bodyMedium),
+                  onTap: () {
+                    context.pop();
+                    context.goto(const OnboardScreen());
+                  },
+                ),
+                ListTile(
+                  leading: const SvgIcon(name: "item_ analyze"),
+                  title: Text("统计", style: Theme.of(context).textTheme.bodyMedium),
+                  onTap: () {
+                    context.pop();
+                    context.goto(const SudokuStatisticsScreen());
+                  },
+                ),
+                ListTile(
+                  leading: const SvgIcon(name: "item_record"),
+                  title: Text("记录", style: Theme.of(context).textTheme.bodyMedium),
+                  onTap: () {
+                    context.pop();
+                    context.goto(const SudokuRecordListScreen());
+                  },
+                ),
+                ListTile(
+                  leading: const SvgIcon(name: "sudoku_share"),
+                  title: Text("分享", style: Theme.of(context).textTheme.bodyMedium),
+                  onTap: () {
+                    context.pop();
+                    context.share(title: '分享一款非常棒的数独游戏, 休闲益智.', subject: "sudoku");
+                  },
+                ),
+                ListTile(
+                  leading: const SvgIcon(name: "item_about"),
+                  title: Text("关于", style: Theme.of(context).textTheme.bodyMedium),
+                  onTap: () {
+                    context.pop();
+                    context.goto(const SudokuAboutScreen());
+                  },
+                ),
+                ListTile(
+                  leading: const SvgIcon(name: "item_feedback"),
+                  title: Text("意见反馈", style: Theme.of(context).textTheme.bodyMedium),
+                  onTap: () async {
+                    context.pop();
+                    final Uri mail = Uri(
+                      scheme: 'mailto',
+                      path: 'gohuhx@gmail.com',
+                      query: 'subject=[数独]意见反馈&body=意见反馈:',
+                    );
+                    if (await canLaunchUrl(mail)) {
+                      await launchUrl(mail);
+                    } else {
+                      Fluttertoast.showToast(msg: 'Can not open email app.');
+                    }
+                  },
+                ),
+                ListTile(
+                  leading: const SvgIcon(name: "sudoku_setting"),
+                  title: Text("设置", style: Theme.of(context).textTheme.bodyMedium),
+                  onTap: () {
+                    context.pop();
+                    context.goto(const SudokuSettingScreen());
+                  },
+                ),
+              ],
+            ),
           ),
         ],
       ),
