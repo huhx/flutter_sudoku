7e25e13269ff4397a16399edcf5b5db49ae07865
hongxiang <gohuhx@163.com>
2023-02-12 21:53:11 +0800

    [huhx] -feat: optimize the success and failed screen

--- lib/business/home/sudoku_failed_screen.dart
+++ lib/business/home/sudoku_failed_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sudoku/business/sudoku/sudoku_notifier.dart';
 import 'package:flutter_sudoku/common/context_extension.dart';
+import 'package:flutter_sudoku/component/appbar_back_button.dart';
 import 'package:flutter_sudoku/component/svg_icon.dart';
 import 'package:flutter_sudoku/provider/error_count_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -15,24 +16,32 @@ class SudokuFailedScreen extends ConsumerWidget {
     final retryCount = ref.watch(errorCountProvider);
 
     return Scaffold(
-      body: Column(
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const SvgIcon(name: "sudoku_failed", size: 64),
-          const Text("挑战失败", style: TextStyle(fontSize: 24, color: Colors.amber)),
-          Text(sudokuNotifier.difficulty.label),
-          Text(sudokuNotifier.dateString),
-          Text("错误：${sudokuNotifier.retryCount}/$retryCount"),
-          TextButton(
-            onPressed: () async {
-              await sudokuNotifier.reset();
-              if (context.mounted) context.pop();
-            },
-            child: const Text("再次挑战"),
-          ),
-        ],
+      appBar: AppBar(
+        leading: const AppbarBackButton(),
+        title: const Text("挑战失败"),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SvgIcon(name: "sudoku_failed", size: 64),
+            const SizedBox(height: 16),
+            Text(sudokuNotifier.difficulty.label),
+            const SizedBox(height: 6),
+            Text(sudokuNotifier.dateString),
+            const SizedBox(height: 6),
+            Text("错误：${sudokuNotifier.retryCount}/$retryCount"),
+            const SizedBox(height: 6),
+            OutlinedButton(
+              onPressed: () async {
+                await sudokuNotifier.reset();
+                if (context.mounted) context.pop();
+              },
+              child: const Text("再次挑战"),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/business/home/sudoku_success_screen.dart
+++ lib/business/home/sudoku_success_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sudoku/business/sudoku/sudoku_notifier.dart';
 import 'package:flutter_sudoku/common/context_extension.dart';
+import 'package:flutter_sudoku/component/appbar_back_button.dart';
 import 'package:flutter_sudoku/component/svg_icon.dart';
 import 'package:flutter_sudoku/provider/error_count_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -15,22 +16,33 @@ class SudokuSuccessScreen extends ConsumerWidget {
     final retryCount = ref.watch(errorCountProvider);
 
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const SvgIcon(name: "sudoku_success", size: 64),
-          const Text("挑战成功", style: TextStyle(fontSize: 24, color: Colors.amber)),
-          Text(sudokuNotifier.difficulty.label),
-          Text(sudokuNotifier.dateString),
-          Text("错误：${sudokuNotifier.retryCount}/$retryCount"),
-          TextButton(
-            onPressed: () async {
-              await sudokuNotifier.next();
-              if (context.mounted) context.pop();
-            },
-            child: const Text("下一关"),
-          ),
-        ],
+      appBar: AppBar(
+        leading: const AppbarBackButton(),
+        title: const Text("挑战成功"),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SvgIcon(name: "sudoku_success", size: 64),
+            const SizedBox(height: 16),
+            Text("难度: ${sudokuNotifier.difficulty.label}"),
+            const SizedBox(height: 6),
+            Text("用时: ${sudokuNotifier.dateString}"),
+            const SizedBox(height: 6),
+            Text("错误：${sudokuNotifier.retryCount}/$retryCount"),
+            const SizedBox(height: 6),
+            OutlinedButton(
+              onPressed: () async {
+                await sudokuNotifier.next();
+                if (context.mounted) context.pop();
+              },
+              child: const Text("下一关"),
+            ),
+          ],
+        ),
       ),
     );
   }
