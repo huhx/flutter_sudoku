77ad15425e5fd478598cf0b2ee6c7ad3abacd120
hongxiang <gohuhx@163.com>
2023-03-04 12:19:04 +0800

    [huhx] -feat: set selection in ErrorCountItem and TipCountItem

--- lib/business/setting/tipcount/tip_count_item.dart
+++ lib/business/setting/tipcount/tip_count_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_sudoku/provider/tip_count_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -10,13 +11,17 @@ class TipCountItem extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final tipCount = ref.watch(tipCountProvider);
+    final userTextController = useTextEditingController()
+      ..text = tipCount.toString()
+      ..selection = TextSelection.fromPosition(const TextPosition(offset: 1));
 
     return CupertinoListTile(
       backgroundColor: Theme.of(context).colorScheme.background,
       title: Text('提醒次数', style: Theme.of(context).textTheme.bodyLarge),
       trailing: Flexible(
         child: TextFormField(
-          initialValue: "$tipCount",
+          controller: userTextController,
+          onTap: () => userTextController.selection = TextSelection.fromPosition(const TextPosition(offset: 1)),
           textAlign: TextAlign.end,
           autofocus: false,
           keyboardType: TextInputType.number,
