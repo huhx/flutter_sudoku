224f6762ebe2d28cf57fae0131056ac9043b8f1a
hongxiang <gohuhx@163.com>
2023-04-23 15:24:29 +0800

    [huhx] -feat: remove QUERY_ALL_PACKAGES permission

--- lib/business/home/sudoku_drawer.dart
+++ lib/business/home/sudoku_drawer.dart
@@ -47,15 +47,14 @@ class SudokuDrawer extends StatelessWidget {
                     context.goto(const SudokuRecordListScreen());
                   },
                 ),
-                if (sudokuConfig.isMember)
-                  ListTile(
-                    leading: const SvgIcon(name: "sudoku_share"),
-                    title: Text("分享", style: Theme.of(context).textTheme.bodyMedium),
-                    onTap: () {
-                      context.pop();
-                      context.share(title: '分享一款非常棒的数独游戏, 休闲益智.', subject: "sudoku");
-                    },
-                  ),
+                ListTile(
+                  leading: const SvgIcon(name: "sudoku_share"),
+                  title: Text("分享", style: Theme.of(context).textTheme.bodyMedium),
+                  onTap: () {
+                    context.pop();
+                    context.share(title: '分享一款非常棒的数独游戏, 休闲益智.', subject: "sudoku");
+                  },
+                ),
                 ListTile(
                   leading: const SvgIcon(name: "item_about"),
                   title: Text("关于", style: Theme.of(context).textTheme.bodyMedium),
@@ -64,23 +63,24 @@ class SudokuDrawer extends StatelessWidget {
                     context.goto(const SudokuAboutScreen());
                   },
                 ),
-                ListTile(
-                  leading: const SvgIcon(name: "item_feedback"),
-                  title: Text("意见反馈", style: Theme.of(context).textTheme.bodyMedium),
-                  onTap: () async {
-                    context.pop();
-                    final Uri mail = Uri(
-                      scheme: 'mailto',
-                      path: 'gohuhx@gmail.com',
-                      query: 'subject=[数独]意见反馈&body=意见反馈:',
-                    );
-                    if (await canLaunchUrl(mail)) {
-                      await launchUrl(mail);
-                    } else {
-                      Fluttertoast.showToast(msg: 'Can not open email app.');
-                    }
-                  },
-                ),
+                if (sudokuConfig.isMember)
+                  ListTile(
+                    leading: const SvgIcon(name: "item_feedback"),
+                    title: Text("意见反馈", style: Theme.of(context).textTheme.bodyMedium),
+                    onTap: () async {
+                      context.pop();
+                      final Uri mail = Uri(
+                        scheme: 'mailto',
+                        path: 'gohuhx@gmail.com',
+                        query: 'subject=[数独]意见反馈&body=意见反馈:',
+                      );
+                      if (await canLaunchUrl(mail)) {
+                        await launchUrl(mail);
+                      } else {
+                        Fluttertoast.showToast(msg: 'Can not open email app.');
+                      }
+                    },
+                  ),
                 ListTile(
                   leading: const SvgIcon(name: "sudoku_setting"),
                   title: Text("设置", style: Theme.of(context).textTheme.bodyMedium),
