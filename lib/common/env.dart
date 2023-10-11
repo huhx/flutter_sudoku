fb65db727d45e68a45c1a8bc704fd39e6bca7081
hongxiang <gohuhx@163.com>
2023-10-11 21:32:59 +0800

    [huhx] -feat: fix the env issue

--- lib/common/env.dart
+++ lib/common/env.dart
@@ -5,5 +5,5 @@ part 'env.g.dart';
 @Envied(path: '.env')
 abstract class Env {
   @EnviedField(varName: 'sudokuUrl', obfuscate: true)
-  static final sudokuUrl = _Env.sudokuUrl;
+  static final String sudokuUrl = _Env.sudokuUrl;
 }
